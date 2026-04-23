2786261a412246eb643373e7f407625e4415302b
Rose, Ian Christian <ianrose@iu.edu>
2026-04-23 09:52:43 -0400

    oops forgot to switch off local :p

--- host.js
+++ host.js
@@ -1,2 +1,2 @@
-export const host = "http://localhost:8080/api";
-//export const host = "https://observationtracker-ianrose.onrender.com/api";
+//export const host = "http://localhost:8080/api";
+export const host = "https://observationtrackerweek2.onrender.com/api";
